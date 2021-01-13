1f74569bd6af01e0adf0d59639049ceec2de42d9
Andrew Colin Kissa <andrew@topdog.za.net>
2021-01-13 10:42:58 +0200

    TST: Add tests

--- intercept_test.go
+++ intercept_test.go
@@ -1,1 +1,213 @@
 package intercept
+
+import (
+	"context"
+	"testing"
+
+	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
+	"github.com/coredns/coredns/plugin/test"
+	"github.com/miekg/dns"
+)
+
+func NewTestControllerWithZones(input string, zones []string) *caddy.Controller {
+	ctr := caddy.NewTestController("dns", input)
+	ctr.ServerBlockKeys = append(ctr.ServerBlockKeys, zones...)
+	return ctr
+}
+
+type testResponseWriter struct {
+	test.ResponseWriter
+	Rcode int
+}
+
+func (t *testResponseWriter) setRemoteIP(ip string) {
+	t.RemoteIP = ip
+}
+
+// WriteMsg implement dns.ResponseWriter interface.
+func (t *testResponseWriter) WriteMsg(m *dns.Msg) error {
+	t.Rcode = m.Rcode
+	return nil
+}
+
+func TestInterceptServeDNS(t *testing.T) {
+	type args struct {
+		domain   string
+		sourceIP string
+		qtype    uint16
+		ctype    uint16
+	}
+	tests := []struct {
+		name     string
+		config   string
+		zones    []string
+		sourceIP string
+		tc       test.Case
+		wantErr  bool
+	}{
+		{
+			"Incercept1",
+			`intercept example.org {
+				record 3600 IN A 127.0.0.2 net 192.168.0.0/16
+			}`,
+			[]string{},
+			"192.168.0.2",
+			test.Case{
+				Qname: "www.example.org.",
+				Qtype: dns.TypeA,
+				Answer: []dns.RR{
+					test.A("www.example.org. 3600	IN	A 127.0.0.2"),
+				},
+			},
+			false,
+		},
+		{
+			"Incercept2",
+			`intercept example.org {
+				record 3600 IN AAAA ::2 net 2001:db8:abcd:0012::0/64
+			}`,
+			[]string{},
+			"2001:db8:abcd:0012::1230",
+			test.Case{
+				Qname: "www.example.org.",
+				Qtype: dns.TypeAAAA,
+				Answer: []dns.RR{
+					test.AAAA("www.example.org. 3600	IN	AAAA ::2"),
+				},
+			},
+			false,
+		},
+		{
+			"Incercept3",
+			`intercept 1.168.192.in-addr.arpa {
+				record 600 IN PTR www.example.org. net 192.168.0.0/16
+			}`,
+			[]string{},
+			"192.168.0.2",
+			test.Case{
+				Qname: "1.1.168.192.in-addr.arpa.",
+				Qtype: dns.TypePTR,
+				Answer: []dns.RR{
+					test.PTR("1.1.168.192.in-addr.arpa. 600	IN	PTR www.example.org."),
+				},
+			},
+			false,
+		},
+		{
+			"Incercept4",
+			`intercept example.org {
+				record 600 IN TXT "v=spf1 ip4:192.168.0.0/16 -all" net 192.168.0.0/16
+			}`,
+			[]string{},
+			"192.168.0.2",
+			test.Case{
+				Qname: "example.org.",
+				Qtype: dns.TypeTXT,
+				Answer: []dns.RR{
+					test.TXT("example.org. 600	IN	TXT \"v=spf1 ip4:192.168.0.0/16 -all\""),
+				},
+			},
+			false,
+		},
+		{
+			"Incercept5",
+			`intercept example.org {
+				record 600 IN NS ns1.example.org. net 192.168.0.0/16
+			}`,
+			[]string{},
+			"192.168.0.2",
+			test.Case{
+				Qname:  "example.org.",
+				Qtype:  dns.TypeNS,
+				Answer: []dns.RR{},
+			},
+			false,
+		},
+		{
+			"Incercept6",
+			`intercept example.org {
+				record 600 IN A 127.0.0.2 net 192.168.0.0/16
+			}`,
+			[]string{},
+			"192.168.1.2",
+			test.Case{
+				Qname:  "example.com.",
+				Qtype:  dns.TypeA,
+				Answer: []dns.RR{},
+			},
+			false,
+		},
+		{
+			"Incercept7",
+			`intercept example.org {
+				record 600 IN A 127.0.0.2 net 192.168.0.0/16
+			}`,
+			[]string{},
+			"192.168.0.2",
+			test.Case{
+				Qname:  "www.example.org.",
+				Qtype:  dns.TypeAAAA,
+				Answer: []dns.RR{},
+			},
+			false,
+		},
+		{
+			"Incercept8",
+			`intercept example.org {
+				record 600 IN A 127.0.0.2 net 192.168.0.0/16
+			}`,
+			[]string{},
+			"10.0.0.1",
+			test.Case{
+				Qname:  "example.org.",
+				Qtype:  dns.TypeA,
+				Answer: []dns.RR{},
+			},
+			false,
+		},
+		{
+			"Incercept9",
+			`intercept example.org {
+				record 3600 IN A 127.0.0.2 127.0.0.3 net 192.168.0.0/16
+			}`,
+			[]string{},
+			"192.168.0.2",
+			test.Case{
+				Qname: "www.example.org.",
+				Qtype: dns.TypeA,
+				Answer: []dns.RR{
+					test.A("www.example.org. 3600	IN	A 127.0.0.2"),
+					test.A("www.example.org. 3600	IN	A 127.0.0.3"),
+				},
+			},
+			false,
+		},
+	}
+	ctx := context.Background()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctr := NewTestControllerWithZones(tt.config, tt.zones)
+			i, err := parse(ctr)
+			i.Next = test.NextHandler(dns.RcodeSuccess, nil)
+			if err != nil {
+				t.Errorf("Error: Cannot intercept config: %v", err)
+				return
+			}
+			w := &testResponseWriter{}
+			w.setRemoteIP(tt.sourceIP)
+			rec := dnstest.NewRecorder(w)
+			m := tt.tc.Msg()
+			_, err = i.ServeDNS(ctx, rec, m)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Error: intercept.ServeDNS() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if resp := rec.Msg; rec.Msg != nil {
+				if err := test.SortAndCheck(resp, tt.tc); err != nil {
+					t.Error(err)
+				}
+			}
+		})
+	}
+}
--- setup_test.go
+++ setup_test.go
@@ -16,144 +16,186 @@ func TestSetup(t *testing.T) {
 		{
 			"Intercept 1",
 			`intercept {
-				IN A 127.0.0.2 net 192.168.0.0/16
+				record 3600 IN A 127.0.0.2 net 192.168.0.0/16
 			}`,
 			false,
 		},
 		{
 			"Intercept 2",
 			`intercept {
-				IN * 127.0.0.2 net 192.168.0.0/16
+				record 3600 IN * 127.0.0.2 net 192.168.0.0/16
 			}`,
 			false,
 		},
 		{
 			"Intercept 3",
 			`intercept {
-				IN A 127.0.0.2 net *
+				record 3600 IN A 127.0.0.2 net *
 			}`,
 			false,
 		},
 		{
 			"Intercept 4",
 			`intercept {
-				IN TXT "The sender is not allowed to query this server" net 192.168.1.0/24
+				record 3600 IN TXT "The sender is not allowed to query this server" net 192.168.1.0/24
 			}`,
 			false,
 		},
 		{
 			"Intercept wildcards",
 			`intercept {
-				IN * 127.0.0.2 net 192.168.0.0/16
-				IN * 127.0.0.2 net *
+				record 3600 IN * 127.0.0.2 net 192.168.0.0/16
+				record 3600 IN * 127.0.0.2 net *
 			}`,
 			false,
 		},
 		{
 			"fine-grained 1",
 			`intercept a.example.org {
-				IN * 127.0.0.2 net 192.168.1.0/24
+				record 3600 IN * 127.0.0.2 net 192.168.1.0/24
 			}`,
 			false,
 		},
 		{
 			"fine-grained 2",
 			`intercept a.example.org {
-				IN * 127.0.0.2 net 192.168.1.0/24
+				record 3600 IN * 127.0.0.2 net 192.168.1.0/24
 			}
 			intercept b.example.org {
-				IN * 127.0.0.2 net 192.168.2.0/24
+				record 3600 IN * 127.0.0.2 net 192.168.2.0/24
 			}`,
 			false,
 		},
 		{
 			"Multiple Networks 1",
 			`intercept example.org {
-				IN * 127.0.0.2 net 192.168.1.0/24 192.168.3.0/24
+				record 3600 IN * 127.0.0.2 net 192.168.1.0/24 192.168.3.0/24
 			}`,
 			false,
 		},
 		{
 			"Multiple Qtypes 1",
 			`intercept example.org {
-				IN TXT ANY 127.0.0.2 net 192.168.3.0/24
+				record 3600 IN TXT ANY 127.0.0.2 net 192.168.10.0/24
 			}`,
 			true,
 		},
 		{
+			"Multiple answers 1",
+			`intercept {
+				record 3600 IN A 127.0.0.2 127.0.0.3 net 192.168.0.0/16
+			}`,
+			false,
+		},
+		{
 			"Missing argument 1",
 			`intercept {
-				A 127.0.0.2 net 192.168.0.0/16
+				record 3600 A 127.0.0.2 net 192.168.0.0/16
 			}`,
 			true,
 		},
 		{
 			"Missing argument 2",
 			`intercept {
-				IN 127.0.0.2 net 192.168.0.0/16
+				record 3600 IN 127.0.0.2 net 192.168.0.0/16
 			}`,
 			true,
 		},
 		{
 			"Missing argument 3",
 			`intercept {
-				IN
+				record 3600 IN
 			}`,
 			true,
 		},
 		{
 			"Missing argument 4",
 			`intercept {
-				IN A
+				record 3600 IN A
 			}`,
 			true,
 		},
 		{
 			"Missing argument 5",
 			`intercept {
-				IN A net
+				record 3600 IN A net
 			}`,
 			true,
 		},
 		{
 			"Missing argument 6",
 			`intercept {
-				IN A 127.0.0.2 net
+				record 3600 IN A 127.0.0.2 net
+			}`,
+			true,
+		},
+		{
+			"Missing argument 7",
+			`intercept {
+				record
+			}`,
+			true,
+		},
+		{
+			"Missing argument 8",
+			`intercept {
+				record 3600
 			}`,
 			true,
 		},
 		{
 			"Illegal argument 1",
 			`intercept {
-				IN ABC 127.0.0.2 net 192.168.0.0/16
+				record 3600 IN ABC 127.0.0.2 net 192.168.0.0/16
 			}`,
 			true,
 		},
 		{
 			"Illegal argument 2",
 			`intercept {
-				blck A 127.0.0.2 net 192.168.0.0/16
+				record 3600 blck A 127.0.0.2 net 192.168.0.0/16
 			}`,
 			true,
 		},
 		{
 			"Illegal argument 3",
 			`intercept {
-				IN A 127.0.0.2 net 192.168.0/16
+				record 3600 IN A 127.0.0.2 net 192.168.0/16
 			}`,
 			true,
 		},
 		{
 			"Illegal argument 4",
 			`intercept {
-				IN A 127.0.0.2 net 192.168.0.0/33
+				record 3600 IN A 127.0.0.2 net 192.168.0.0/33
 			}`,
 			true,
 		},
 		{
 			"Illegal argument 5",
 			`intercept {
-				IN A 127.0.0.260 net 192.168.0.0/33
+				record 3600 IN A 127.0.0.260 net 192.168.0.0/33
+			}`,
+			true,
+		},
+		{
+			"Illegal argument 6",
+			`intercept {
+				records 3600 IN A 127.0.0.2 net 192.168.0.0/16
+			}`,
+			true,
+		},
+		{
+			"Illegal argument 7",
+			`intercept {
+				record XX IN A 127.0.0.2 net 192.168.0.0/16
+			}`,
+			true,
+		},
+		{
+			"Illegal argument 8",
+			`intercept {
+				record 65536 IN A 127.0.0.2 net 192.168.0.0/16
 			}`,
 			true,
 		},
@@ -161,59 +203,59 @@ func TestSetup(t *testing.T) {
 		{
 			"Intercept 1 IPv6",
 			`intercept {
-				IN A ::2 net 2001:0db8:85a3:0000:0000:8a2e:0370:7334
+				record 3600 IN A ::2 net 2001:0db8:85a3:0000:0000:8a2e:0370:7334
 			}`,
 			false,
 		},
 		{
 			"Intercept 2 IPv6",
 			`intercept {
-				IN * ::2 net 2001:db8:85a3::8a2e:370:7334
+				record 3600 IN * ::2 net 2001:db8:85a3::8a2e:370:7334
 			}`,
 			false,
 		},
 		{
 			"Intercept 3 IPv6",
 			`intercept {
-				IN A ::2
+				record 3600 IN A ::2
 			}`,
 			false,
 		},
 		{
 			"fine-grained 1 IPv6",
 			`intercept a.example.org {
-				IN A ::2 net 2001:db8:abcd:0012::0/64
+				record 3600 IN A ::2 net 2001:db8:abcd:0012::0/64
 			}`,
 			false,
 		},
 		{
 			"fine-grained 2 IPv6",
 			`intercept a.example.org {
-				IN A ::2 net 2001:db8:abcd:0012::0/64
+				record 3600 IN A ::2 net 2001:db8:abcd:0012::0/64
 			}
 			intercept b.example.org {
-				IN A ::2 net 2001:db8:abcd:0013::0/64
+				record 3600 IN A ::2 net 2001:db8:abcd:0013::0/64
 			}`,
 			false,
 		},
 		{
 			"Multiple Networks 1 IPv6",
 			`intercept example.org {
-				IN A ::2 net 2001:db8:abcd:0012::0/64 2001:db8:85a3::8a2e:370:7334/64
+				record 3600 IN A ::2 net 2001:db8:abcd:0012::0/64 2001:db8:85a3::8a2e:370:7334/64
 			}`,
 			false,
 		},
 		{
 			"Illegal argument 1 IPv6",
 			`intercept {
-				IN A ::2 net 2001::85a3::8a2e:370:7334
+				record 3600 IN A ::2 net 2001::85a3::8a2e:370:7334
 			}`,
 			true,
 		},
 		{
 			"Illegal argument 2 IPv6",
 			`intercept {
-				IN A ::2 net 2001:db8:85a3:::8a2e:370:7334
+				record 3600 IN A ::2 net 2001:db8:85a3:::8a2e:370:7334
 			}`,
 			true,
 		},
